51ccbae591adc1fabab31a708a1a43cf6665c697
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-10-18 19:07:07 +0300

    HW-2

---
+++ ConsoleApp/Pool.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    public sealed class Pool : IDisposable
+    {
+        private readonly LinkedList<Thread> _workers;
+        private readonly LinkedList<Action> _tasks = new LinkedList<Action>();
+        private bool _disallowAdd;
+        private bool _disposed;
+
+        public Pool(int poolSize)
+        {
+            _workers = new LinkedList<Thread>();
+            for (var i = 0; i < poolSize; ++i)
+            {
+                var worker = new Thread(Worker) { Name = string.Concat("Worker ", i) };
+                worker.Start();
+                _workers.AddLast(worker);
+            }
+        }
+
+        public void Dispose()
+        {
+            var waitForThreads = false;
+            lock (_tasks)
+            {
+                if (!_disposed)
+                {
+                    GC.SuppressFinalize(this);
+
+                    _disallowAdd = true; 
+                    while (_tasks.Count > 0)
+                    {
+                        Monitor.Wait(_tasks);
+                    }
+
+                    _disposed = true;
+                    Monitor.PulseAll(_tasks); 
+                    waitForThreads = true;
+                }
+            }
+            if (waitForThreads)
+            {
+                foreach (var worker in _workers)
+                {
+                    worker.Join();
+                }
+            }
+        }
+
+        public void QueueTask(Action task)
+        {
+            lock (_tasks)
+            {
+                if (_disallowAdd) { throw new InvalidOperationException("This Pool instance is in the process of being disposed, can't add anymore"); }
+                if (_disposed) { throw new ObjectDisposedException("This Pool instance has already been disposed"); }
+                _tasks.AddLast(task);
+                Monitor.PulseAll(_tasks); 
+            }
+        }
+
+        private void Worker()
+        {
+            Action task = null;
+            while (true) 
+            {
+                lock (_tasks) 
+                {
+                    while (true) 
+                    {
+                        if (_disposed)
+                        {
+                            return;
+                        }
+                        if (null != _workers.First && object.ReferenceEquals(Thread.CurrentThread, _workers.First.Value) && _tasks.Count > 0) 
+                        {
+                            task = _tasks.First.Value;
+                            _tasks.RemoveFirst();
+                            _workers.RemoveFirst();
+                            Monitor.PulseAll(_tasks); 
+                            break; 
+                        }
+                        Monitor.Wait(_tasks); 
+                    }
+                }
+
+                task(); 
+                lock (_tasks)
+                {
+                    _workers.AddLast(Thread.CurrentThread);
+                }
+                task = null;
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var pool = new Pool(5))
+            {
+                Action<int> action = (index =>
+                {
+
+                    Console.WriteLine($"Start: ThreadId - {Thread.CurrentThread.ManagedThreadId} - number {index}");
+                    Thread.Sleep(200);
+                    Console.WriteLine($"End: ThreadId - {Thread.CurrentThread.ManagedThreadId} - number {index}");
+                });
+
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    pool.QueueTask(() => action(i));
+                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}

48567c477f448388c24b966a9dfc4f0aaf3973b0
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-10-25 12:03:43 +0300

    ДЗ 3

---
+++ ConsoleApp/MainClasses/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.MainClasses
+{
+    public class Country
+    {
+        public string countryName;
+
+        public Country(string countryName)
+        {
+            this.countryName = countryName;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,57 @@
 ﻿using System;
 using System.Threading;
+using ConsoleApp.MainClasses;
 
 namespace ConsoleApp
 {
+    // Используя знания о паттернах, выберите приглянувшийся вам
     class Program
     {
         static void Main(string[] args)
         {
+            //Паттерн прототип
+            PostPackege postPackege_1 = new PostPackege();
+            postPackege_1.id = 1;
+            postPackege_1.name = "Name_1";
+            postPackege_1.postDate = Convert.ToDateTime("2021-10-05");
+            postPackege_1.country = new Country("Country name - 1");
+
+          
+            PostPackege postPackege_2 = (PostPackege)postPackege_1.ShallowCopy();
+
+            PostPackege postPackege_3 = (PostPackege)postPackege_1.DeepCopy();
+
             
-            using (var pool = new Pool(5))
-            {
-                Action<int> action = (index =>
-                {
+            Console.WriteLine("Изначальные значения");
+            Console.WriteLine("   postPackege_1: ");
+            PrintValues(postPackege_1);
+            Console.WriteLine("   postPackege_2:");
+            PrintValues(postPackege_2);
+            Console.WriteLine("   postPackege_3:");
+            PrintValues(postPackege_3);
 
-                    Console.WriteLine($"Start: ThreadId - {Thread.CurrentThread.ManagedThreadId} - number {index}");
-                    Thread.Sleep(200);
-                    Console.WriteLine($"End: ThreadId - {Thread.CurrentThread.ManagedThreadId} - number {index}");
-                });
+            
+            postPackege_1.id = 2;
+            postPackege_1.postDate = Convert.ToDateTime("2022-10-05");
+            postPackege_1.name = "Name_2";
+            postPackege_1.country.countryName = "Country name - 2";
+            Console.WriteLine("\nВсе значения после изменения прототипа:");
+            Console.WriteLine("   postPackege_1 instance values: ");
+            PrintValues(postPackege_1);
+            Console.WriteLine("   postPackege_2 содержит ссылку без изменений:");
+            PrintValues(postPackege_2);
+            Console.WriteLine("   postPackege_3 полное копирование:");
+            PrintValues(postPackege_3);
 
-                for (int i = 0; i < 10; i++)
-                {
-                    
-                    pool.QueueTask(() => action(i));
-                    
-                }
-            }
 
             Console.ReadKey();
         }
-        
+
+        public static void PrintValues(PostPackege packege)
+        {
+            Console.WriteLine("      Id: {0}, Name: {1}, PostDate: {2:MM/dd/yy}",
+                packege.id, packege.name, packege.postDate);
+            Console.WriteLine("      Country: {0:d}", packege.country.countryName);
+        }
     }
 }

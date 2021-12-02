96af255db7ff961359db1def1563d80c6e85719e
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-12-02 09:43:31 +0300

    ДЗ 8

---
+++ MVC App/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() {Name = "Name#1", Position = "Position#1"},
+                new Employee() {Name = "Name#2", Position = "Position#2"},
+                new Employee() {Name = "Name#3", Position = "Position#3"},
+                new Employee() {Name = "Name#4", Position = "Position#4"}
+            };
+            OfficeViewModel office = new OfficeViewModel(employees);
+            return View(office);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ MVC App/Models/OfficeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_App.Models
+{
+    public class OfficeViewModel
+    {
+        private readonly List<Employee> _employees;
+
+        public OfficeViewModel(List<Employee> employees)
+        {
+            _employees = employees;
+        }
+
+        public int EmployeeCount => _employees.Count;
+
+        public List<Employee> Employees => _employees;
+    }
+}
